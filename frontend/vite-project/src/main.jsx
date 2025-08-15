536cc82d3ff2d16428a1becf1f4b94e3756d6b8e
Apurv Singh <rkumarsingh003@gmail.com>
2025-08-16 02:14:54 +0530

    UI Enhancement for Room/Session Joining Page

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -3,7 +3,7 @@ window.global = window;
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import './main.css';
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(

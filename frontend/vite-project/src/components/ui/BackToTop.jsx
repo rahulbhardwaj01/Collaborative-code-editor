445ff7b0a1f2090d875e8057298f478ae4e85a70
Priyanshi sahu <priyanshi4263@gmail.com>
2025-08-17 07:09:56 +0530

    minor fix

--- frontend/vite-project/src/components/ui/BackToTop.jsx
+++ frontend/vite-project/src/components/ui/BackToTop.jsx
@@ -6,7 +6,6 @@ const BackToTop = () => {
     const [isVisible, setIsVisible] = useState(false);
 
     const handleScroll = () => {
-        console.log('Scroll Y:', window.scrollY);
         if (window.scrollY > 100) {
             setIsVisible(true);
         } else {
